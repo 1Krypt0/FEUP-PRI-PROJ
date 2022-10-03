6be313e37a6c633ec99edc7b73f1dd96e79cb1fd
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-10-03 15:51:14 +0100

    add try except statement so it doesnt halt when not finding the article

--- src/fill-missing.py
+++ src/fill-missing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from newspaper import Article
+from newspaper import Article, ArticleException
 
 df = pd.read_csv('data/spacenews.csv')
 
@@ -12,11 +12,13 @@ urls = df["url"][empty_content]
 # Parse the original article and fill up the rest of the table
 for url, index in zip(urls, empty_content):
     article = Article(url)
-    article.download()
-    article.parse()
+    try:
+        article.download()
+        article.parse()
+    except ArticleException as exception:
+        print(f"Could not find article relative to URL {url}. Index was {index}")
+        continue
     df["content"][index] = article.text.replace('\n', ' ')
     print(f"Parsed news item {index}")
 
 df.to_csv('data/spacenews_filled.csv')
-
-
