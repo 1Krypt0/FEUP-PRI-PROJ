0b3c3d804d6928e8b34596c3e7e88acc53c43a6f
eukia <ritam.mendes@gmail.com>
2022-10-06 02:19:30 +0100

    Fix frequent_authors

--- src/statistical_analysis.R
+++ src/statistical_analysis.R
@@ -1,4 +1,7 @@
 
+# library
+library(wordcloud2)
+
 file <- "data/spacenews_filled.csv"
 data <- read.csv(file)
 
@@ -14,7 +17,9 @@ hist(data_publishing_year,
 
 # Frequency of authors (at least 100 articles)
 table_authors <- table(data$author)
-frequent_authors <- data[data$author %in% names(tt[tt > 100]), ]
+frequent_authors <- data[
+    data$author %in% names(table_authors[table_authors > 100]),
+]
 barplot(sort(table(frequent_authors$author), decreasing = TRUE),
     las = 2, cex.names = 0.6,
     main = "Frequency of articles by authors with at least 100 articles"
