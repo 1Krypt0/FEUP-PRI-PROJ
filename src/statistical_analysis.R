74af934f1f7a9e8372d7f7ad370976123e42e0c1
eukia <ritam.mendes@gmail.com>
2022-10-06 02:06:55 +0100

    Add authors with most news published barplot

--- src/statistical_analysis.R
+++ src/statistical_analysis.R
@@ -1,8 +1,21 @@
 
 file <- "data/spacenews_filled.csv"
 data <- read.csv(file)
-formated_date <- as.numeric(format(as.Date(data$date, "%B %d, %Y"), "%Y"))
-hist(formated_date,
-    breaks = 20, xlab = "Year", xlim = c(2005, 2022),
-    main = "Year of publishing"
+
+# Number of news published each year
+data_publishing_year <- as.numeric(format(
+    as.Date(data$date, "%B %d, %Y"), "%Y"
+))
+hist(data_publishing_year,
+    breaks = 20, xlab = "Year", xlim = c(2005, 2025),
+    main = "Histogram of news' year of publishing"
+)
+
+
+# Frequency of authors (at least 100 articles)
+table_authors <- table(data$author)
+frequent_authors <- data[data$author %in% names(tt[tt > 100]), ]
+barplot(sort(table(frequent_authors$author), decreasing = TRUE),
+    las = 2, cex.names = 0.6,
+    main = "Frequency of articles by authors with at least 100 articles"
 )
