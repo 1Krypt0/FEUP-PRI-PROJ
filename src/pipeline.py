32182f80368b4c99436c18b5ff519a055bb60687
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-10-08 18:13:22 +0100

    Change dataframe name. Add part to remove all nulls

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 from newspaper import Article, ArticleException
+from sqlalchemy.sql import False_
 
-df = pd.read_csv('data/spacenews.csv')
+news = pd.read_csv('data/spacenews.csv')
 
 # Find out where there is no content
-empty_content = np.asarray(np.where(pd.isnull(df["content"])))[0]
+empty_content = np.asarray(np.where(pd.isnull(news['content'])))[0]
 
-urls = df["url"][empty_content]
+urls = news['url'][empty_content]
 
 # Parse the original article and fill up the rest of the table
 for url, index in zip(urls, empty_content):
@@ -18,25 +19,34 @@ for url, index in zip(urls, empty_content):
     except ArticleException as exception:
         print(f"Could not find article relative to URL {url}. Index was {index}")
         continue
-    df["content"][index] = article.text
+    news['content'][index] = article.text.replace('\n', ' ')
     print(f"Parsed news item {index}")
 
-# Remove articles that had no corresponding content online
-df.dropna()
-
 # Drop post excerpt column
-df.drop("postexcerpt", axis=1, inplace=True)
+news.drop("postexcerpt", axis=1, inplace=True)
+
+# Remove articles that had no corresponding content online
+news.dropna(inplace=True)
 
 # Remove authors from this dataset and add it into another
-authors = df["author"]
+authors = news['author']
 authors = authors.unique()
 authors = pd.DataFrame(authors)
 authors.columns = ['name']
 
 for index, author in zip(authors.index, authors['name']):
-    articles = df.index[df['author'] == author].tolist()
-    df['author'][articles] = index
+    articles = news.index[news['author'] == author].tolist()
+    news['author'][articles] = index
 
 authors.to_csv('data/authors.csv')
-df.rename(columns = {'author': 'authorID'}, inplace=True)
-df.to_csv('data/spacenews_clean.csv')
+news.rename(columns = {'author': 'authorID'}, inplace=True)
+news.to_csv('data/spacenews_clean.csv', index=False)
+
+# Without this section this doesn't work, for some reason
+
+clean_news = pd.read_csv('data/spacenews_clean.csv')
+
+# Remove articles that had no corresponding content online
+clean_news.dropna(inplace=True)
+
+clean_news.to_csv('data/spacenews_clean.csv')
