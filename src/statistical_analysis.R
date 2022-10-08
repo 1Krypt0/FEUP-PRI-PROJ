c97a1335cc5f9ede28cde290f2db65ef9f1a9569
eukia <ritam.mendes@gmail.com>
2022-10-08 12:20:09 +0100

    Change image extraction path

--- src/statistical_analysis.R
+++ src/statistical_analysis.R
@@ -1,7 +1,3 @@
-
-# library
-library(wordcloud2)
-
 file <- "data/spacenews_filled.csv"
 data <- read.csv(file)
 
@@ -9,10 +5,14 @@ data <- read.csv(file)
 data_publishing_year <- as.numeric(format(
     as.Date(data$date, "%B %d, %Y"), "%Y"
 ))
+
+png(file = "docs/img/histogram_year.png")
 hist(data_publishing_year,
     breaks = 20, xlab = "Year", xlim = c(2005, 2025),
-    main = "Histogram of news' year of publishing"
+    main = "Histogram of news' year of publishing", col = "lightblue",
+    ylim = c(0, 2000),
 )
+dev.off()
 
 
 # Frequency of authors (at least 100 articles)
@@ -20,7 +20,10 @@ table_authors <- table(data$author)
 frequent_authors <- data[
     data$author %in% names(table_authors[table_authors > 100]),
 ]
+png(file = "docs/img/plot_authors.png")
 barplot(sort(table(frequent_authors$author), decreasing = TRUE),
     las = 2, cex.names = 0.6,
-    main = "Frequency of articles by authors with at least 100 articles"
+    main = "Frequency of articles by authors with at least 100 articles",
+    col = "lightblue"
 )
+dev.off()
