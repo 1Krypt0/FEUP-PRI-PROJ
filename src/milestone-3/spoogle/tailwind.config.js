09af7be46ee928183fc0117688c9a5db6dec1b53
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-12-02 21:50:15 +0000

    chore: add tailwind to project

---
+++ src/milestone-3/spoogle/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        snred: "#ee1c25",
+        snblack: "#303030",
+      },
+    },
+  },
+  plugins: [],
+};
