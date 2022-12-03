cca541b5990574017acba28b51db4e5c2318d48c
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-12-03 16:11:08 +0000

    refactor: move client to new project folder

---
+++ src/milestone-3/spoogle/client/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="flex items-center justify-center">
+    <section class="flex flex-col justify-evenly pb-32 w-1/4 h-3/4">
+      <img src="../assets/logo.svg" class="w-" />
+      <SearchBox />
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts">
+import SearchBox from "../components/SearchBox.vue";
+import axios from "axios";
+</script>
--- src/milestone-3/spoogle/src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import SearchBox from "../components/SearchBox.vue";
-</script>
-
-<template>
-  <main class="flex items-center justify-center">
-    <SearchBox />
-  </main>
-</template>
