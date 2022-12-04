a8657bf7a74331e1ec39f6e0586745d6b607eba2
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-12-04 23:00:58 +0000

    feat: add solr data to express server

--- src/milestone-3/spoogle/server/server.js
+++ src/milestone-3/spoogle/server/server.js
@@ -1,77 +1,49 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
+
+const BASE_URL = "http://localhost:8983/solr/articles/select";
+
+const solr = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+});
+
+async function searchSolr(query) {
+  const response = await solr.get(BASE_URL, {
+    params: {
+      defType: "edismax",
+      indent: true,
+      "q.op": "AND",
+      qf: "title^10 content^5 tags^3 sections^3",
+      rows: 8,
+      start: 0,
+      q: query,
+    },
+  });
+
+  const solrResponse = response.data.response;
+
+  if (solrResponse) {
+    return solrResponse.docs;
+  }
+
+  /*.then((response) => {
+      const solrResponse = response.data.response;
+      if (solrResponse) {
+        const docs = solrResponse.docs;
+        return docs;
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    })*/
+}
 
 const app = express();
 app.use(cors());
+
 const port = 3000;
-
-const results = [
-  {
-    id: "789b8820-b4ce-4548-9af9-845c11e6353b",
-    title: "Astrocast buying Hiber to boost funding and expansion plans",
-    content:
-      "TAMPA, Fla. — Cash-strapped smallsat operator Astrocast announced \
-      plans May 30 to buy Dutch connectivity solutions provider Hiber, which \
-      scrapped plans for its own constellation last year following failures on\
-      its initial satellites.  Switzerland-based Astrocast said the deal would\
-      give it spectrum in the Americas to enable its growing constellation for connecting\
-      Internet of Things (IoT) devices to operate globally. Astrocast plans to issue new shares\
-      to fund the acquisition. Hiber has also agreed to invest around $11 million in Astrocast’s second\
-      stock market listing later this year to help the Swiss startup meet near-term funding needs.  Astrocast hopes to raise 60-80 million euros ($64-86 million) by listing shares on the Euronext Growth Paris\
-      junior stock market in France. The startup said it needs more funds to expand its constellation of 12 satellites — including two test spacecraft — to 20 before the end of 2022, and to 40 in 2023 to improve\
-      coverage.  By 2025, Astrocast aims to be operating 100 satellites in low Earth orbit.  While the first 20 will be three-unit cubesats, Astrocast has said later models could be larger six-unit cubesats. Astrocast\
-      became a public company in August when it raised about $42 million by listing shares on Euronext Growth Oslo in Norway. The operator launched commercially in February for regions outside the Americas which, according to Astrocast\
-      spokesperson Fatima Vigil-Perez, currently receive services with a typical two-hour latency. However, Astrocast estimated in its May 13 annual financial report that it needs to raise 43 million Swiss Francs ($45 million)\
-      in 2022 for expansion plans that would enable it to improve coverage and satellite revisit rates. The company reported a 21.4 million Swiss Franc loss for 2021. Established operators — and other startups that include\
-      SpaceX-owned Swarm  — are also seeking a share of a space-based IoT market that typically uses satellites to connect small devices out the reach of terrestrial networks.  Astrocast says its services are focused on connecting\
-      customers in industrial markets that include agriculture, maritime and utilities. Hiber also targets industrial customers — particularly those in the oil & gas, mining, logistics and agriculture markets — and provides\
-      satellite-connected devices and solutions that enable customers to monitor and track assets in remote locations.  The Dutch company’s business is based on multi-year subscriptions that cover sensors, network hardware, satellite\
-      connectivity and a user interface. In October, Hiber  announced a partnership  to use Inmarsat’s satellites for its IoT solutions after giving up on its own constellation. The Dutch company had surrendered a license to operate\
-      a network of 24 IoT smallsats just weeks before, blaming technical issues on four initial satellites it had deployed and financial issues.  Astrocast said acquiring Hiber would give the Swiss company access to L-band spectrum for\
-      covering the Americas that is part of an agreement with Inmarsat. Astrocast’s satellites already use L-band to provide services. “Our satellites are designed to provide connectivity globally so there’s\
-      no need to wait for the launch of future satellites to provide connectivity in the Americas,” Vigil-Perez said. Astrocast plans to buy Hiber with newly issued shares that currently represent a 16.5% stake\
-      in Astrocast. The acquisition is dependent on Astrocast completing its Euronext Growth Paris stock market listing.",
-    url: "https://spacenews.com/astrocast-buying-hiber-to-boost-funding-and-expansion-plans/",
-    author: "Jason Rainbow",
-    date: "2022-05-31T00:00:00Z",
-    tags: ["astrocast", " hiber"],
-    sections: ["europe"],
-  },
-  {
-    id: "1",
-    title: "Fancy title 1",
-    content: "Fancy content of the very lengthy news article 1",
-    url: "Url 1",
-    author: "Author 1",
-    date: "2022-05-31T00:00:00Z",
-    tags: ["tag 0", "tag1"],
-    sections: ["section0", "section1"],
-  },
-  {
-    id: "2",
-    title: "Fancy title 2",
-    content: "Fancy content of the very lengthy news article 2",
-    url: "Url 2",
-    author: "Author 2",
-    date: "2022-05-31T00:00:00Z",
-    tags: ["tag 0", "tag1"],
-    sections: ["section0", "section1"],
-  },
-  {
-    id: "3",
-    title: "Fancy title 3",
-    content: "Fancy content of the very lengthy news article 3",
-    url: "Url 3",
-    author: "Author 0",
-    date: "2022-05-31T00:00:00Z",
-    tags: ["tag 0", "tag1"],
-    sections: ["section0", "section1"],
-  },
-];
-
-app.get("/", (req, res) => {
-  res.send("Hello, World");
-});
 
 app.get("/article/:id", (req, res) => {
   const id = req.params.id;
@@ -79,8 +51,8 @@ app.get("/article/:id", (req, res) => {
   res.send(article);
 });
 
-app.get("/search", (req, res) => {
-  console.log("Request:", req.query);
+app.get("/search", async (req, res) => {
+  const results = await searchSolr(req.query.q);
   res.send(results);
 });
 
