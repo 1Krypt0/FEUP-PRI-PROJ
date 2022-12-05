a9c359df36ee4fd150db28dcef8bc94a315e0677
eukia <ritam.mendes@gmail.com>
2022-12-05 16:13:15 +0000

    Add frontend for more like this feature

--- src/milestone-3/spoogle/client/src/components/MoreLikeThis.vue
+++ src/milestone-3/spoogle/client/src/components/MoreLikeThis.vue
@@ -1,13 +1,29 @@
 <template>
-  <span
-    class="inline-block bg-slate-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2"
-    >More Like This</span
-  >
+  <div class="inline-block bg-slate-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+    <!-- <router-link to="/mlt" custom v-slot="{ navigate }"> -->
+    <!--   <button @click="navigate" role="link"> -->
+    <!--     More Like This {{ id }} -->
+    <!--   </button> -->
+    <!-- </router-link> -->
+    <form @submit.prevent="getMoreLikeThis">
+      <button role="link">
+        More Like This {{ id }}
+      </button>
+    </form>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
+
 export interface Article {
   id: string;
 }
 const props = defineProps<Article>();
+const router = useRouter()
+function getMoreLikeThis() {
+  router.push({name: "mlt", query: {id: props.id}});
+}
+
+
 </script>
--- src/milestone-3/spoogle/server/server.js
+++ src/milestone-3/spoogle/server/server.js
@@ -3,11 +3,17 @@ const cors = require("cors");
 const axios = require("axios");
 
 const BASE_URL = "http://localhost:8983/solr/articles/select";
+const MLT_BASE_URL = "http://localhost:8983/solr/articles/mlt";
 
 const solr = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
 });
+
+const solrMlt = axios.create({
+  baseURL: MLT_BASE_URL,
+  timeout: 10000,
+})
 
 async function searchSolr(query, page) {
   const response = await solr.get(BASE_URL, {
@@ -35,6 +41,37 @@ async function searchSolr(query, page) {
   }
 }
 
+async function mltSolr(id, page) {
+
+  // console.log("page is ", page);
+
+  // http://localhost:8983/solr/articles/mlt?q=id%3A7e8fff96-96a6-4482-8427-737ee27d70dd
+  let url = "http://localhost:8983/solr/articles/mlt?q=id%3A" + id;
+  console.log(url);
+  console.log("start is", page * 8);
+  // console.log(page);
+  const response = await solrMlt.get(MLT_BASE_URL + '?q=id%3A' + id, {
+    params: {
+      rows: 8,
+      start: page * 8,
+      "mlt.match.include": false,
+    }
+  });
+
+  const solrResponse = response.data.response;
+  console.log(solrResponse);
+  // console.log("sr " + solrResponse.docs);
+  if (solrResponse) {
+    const docs = solrResponse.docs;
+    const numFound = solrResponse.numFound;
+    console.log("Found a total of", numFound);
+    return {
+      docs,
+      numFound,
+    };
+  }
+}
+
 const app = express();
 app.use(cors());
 
@@ -50,6 +87,11 @@ app.get("/article/:id", async (req, res) => {
 
 app.get("/search", async (req, res) => {
   const results = await searchSolr(req.query.q, req.query.page);
+  res.send(results);
+});
+
+app.get("/mlt", async (req, res) => {
+  const results = await mltSolr(req.query.id, req.query.page);
   res.send(results);
 });
 
