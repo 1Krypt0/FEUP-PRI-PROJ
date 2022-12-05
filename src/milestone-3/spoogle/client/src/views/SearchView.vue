4a92d737aae167aa615accbd635fe8a60dc72f8b
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-12-05 00:08:46 +0000

    feat: add pagination to search results
    fix: wrong page updating values when no values are retrieved
    
    fix: pagination results
    
    refactor server methods
    
    fix: problems with axios buffer
    
    fix issue with page total amount
    
    remove filters from UI
    
    remove gitignore files

--- src/milestone-3/spoogle/client/src/views/SearchView.vue
+++ src/milestone-3/spoogle/client/src/views/SearchView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { inject, onBeforeMount, ref } from "vue";
+import { inject, onBeforeMount, onMounted, ref, watch, computed } from "vue";
 import { useRoute } from "vue-router";
 import ResultList from "../components/ResultList.vue";
 import FilterBox from "../components/FilterBox.vue";
@@ -21,20 +21,18 @@ export interface Results {
 }
 
 const api = inject("api") as AxiosInstance;
-const getItems = (query: string) => {
-  api
-    .get("/search", {
-      params: {
-        q: query,
-      },
-    })
-    .then((response) => {
-      results.value = response.data;
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-};
+async function getItems(
+  query: string,
+  page: number
+): Promise<{ numFound: number; docs: Article[] }> {
+  const results = await api.get("/search", {
+    params: {
+      q: query,
+      page: page,
+    },
+  });
+  return results.data;
+}
 
 const results = ref([
   {
@@ -48,22 +46,61 @@ const results = ref([
     sections: [""],
   },
 ]);
+const amount = ref(0);
+const page = ref(0);
 const query = useRoute().query.q as string;
+const bottom = ref(false);
 
-onBeforeMount(() => {
-  getItems(query);
+watch(bottom, (newValue) => {
+  if (newValue) {
+    console.log("At the bottom, fetching more items");
+    getMoreItems(query);
+  }
+});
+
+async function getMoreItems(query: string): Promise<void> {
+  const newResults = await getItems(query, page.value + 1);
+  if (newResults) {
+    results.value.push(...newResults.docs);
+    page.value++;
+  }
+}
+
+onMounted(() => {
+  window.addEventListener("scroll", () => {
+    bottom.value = bottomVisible();
+  });
+});
+
+const total = computed(() => {
+  return amount.value < 10
+    ? amount.value
+    : (page.value + 1) * 10 > amount.value
+    ? amount.value
+    : (page.value + 1) * 10;
+});
+
+const bottomVisible = () => {
+  const scrollY = window.scrollY;
+  const visible = document.documentElement.clientHeight;
+  const pageHeight = document.documentElement.scrollHeight;
+  const bottomOfPage = visible + scrollY >= pageHeight;
+  return bottomOfPage || pageHeight < visible;
+};
+
+onBeforeMount(async () => {
+  const items = await getItems(query, 0);
+  results.value = items.docs;
+  amount.value = items.numFound;
 });
 </script>
 
 <template>
   <div class="flex pt-20">
-    <section class="flex w-1/5 flex-col px-10">
-      <h2 class="text-3xl font-sn pb-10">Filters</h2>
-      <FilterBox />
-    </section>
-    <main class="flex w-3/5 justify-center px-10">
+    <main class="flex flex-col w-3/5 mx-auto justify-center px-10">
+      <p class="self-end">Showing {{ total }} of {{ amount }} results</p>
       <ResultList :results="results" />
+      <section></section>
     </main>
-    <section class="flex w-1/5 flex-col"></section>
   </div>
 </template>
--- src/milestone-3/spoogle/server/server.js
+++ src/milestone-3/spoogle/server/server.js
@@ -1,38 +1,27 @@
-const express = require("express");
-const cors = require("cors");
-const axios = require("axios");
+import cors from "cors";
+import express from "express";
 
-const BASE_URL = "http://localhost:8983/solr/articles/select";
+const BASE_URL = "http://localhost:8983/solr/articles/select?";
 
-const solr = axios.create({
-  baseURL: BASE_URL,
-  timeout: 10000,
-});
-
-async function searchSolr(query, page) {
-  const response = await solr.get(BASE_URL, {
-    params: {
-      defType: "edismax",
-      indent: true,
-      "q.op": "AND",
-      qf: "title^10 content^5 tags^3 sections^3 author date id",
-      rows: 8,
-      start: page * 8,
-      q: query,
+async function searchSolr(params) {
+  const url = BASE_URL + new URLSearchParams(params);
+  const fetchResult = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Credentials": true,
     },
-  });
+  })
+    .then((result) => result.json())
+    .then((data) => {
+      return {
+        docs: data.response.docs,
+        numFound: data.response.numFound,
+      };
+    });
 
-  const solrResponse = response.data.response;
-
-  if (solrResponse) {
-    const docs = solrResponse.docs;
-    const numFound = solrResponse.numFound;
-    console.log("Found a total of", numFound);
-    return {
-      docs,
-      numFound,
-    };
-  }
+  return fetchResult;
 }
 
 const app = express();
@@ -43,13 +32,28 @@ const port = 3000;
 app.get("/article/:id", async (req, res) => {
   const id = req.params.id;
   const query = `id:${id}`;
-  const results = await searchSolr(query, 0);
+  const params = {
+    "q.op": "OR",
+    rows: 1,
+    wt: "json",
+    q: query,
+  };
+  const results = await searchSolr(params);
   const article = results.docs[0];
   res.send(article);
 });
 
 app.get("/search", async (req, res) => {
-  const results = await searchSolr(req.query.q, req.query.page);
+  const params = {
+    q: req.query.q,
+    "q.op": "AND",
+    qf: "title^10 content^5 tags^3 sections^3 author date id",
+    wt: "json",
+    defType: "edismax",
+    rows: 10,
+    start: req.query.page * 10,
+  };
+  const results = await searchSolr(params);
   res.send(results);
 });
 
