a7846eeefa9212e59e0d49bfac662de44946138b
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-12-05 17:00:34 +0000

    refactor server methods

--- src/milestone-3/spoogle/server/server.js
+++ src/milestone-3/spoogle/server/server.js
@@ -9,17 +9,9 @@ const solr = axios.create({
   timeout: 10000,
 });
 
-async function searchSolr(query, page) {
+async function searchSolr(params) {
   const response = await solr.get(BASE_URL, {
-    params: {
-      defType: "edismax",
-      indent: true,
-      "q.op": "AND",
-      qf: "title^10 content^5 tags^3 sections^3 author date id",
-      rows: 8,
-      start: page * 8,
-      q: query,
-    },
+    params: params,
   });
 
   const solrResponse = response.data.response;
@@ -27,7 +19,6 @@ async function searchSolr(query, page) {
   if (solrResponse) {
     const docs = solrResponse.docs;
     const numFound = solrResponse.numFound;
-    console.log("Found a total of", numFound);
     return {
       docs,
       numFound,
@@ -43,13 +34,28 @@ const port = 3000;
 app.get("/article/:id", async (req, res) => {
   const id = req.params.id;
   const query = `id:${id}`;
-  const results = await searchSolr(query, 0);
+  const params = {
+    "q.op": "OR",
+    rows: 1,
+    wt: "json",
+    q: query,
+  };
+  const results = await searchSolr(params);
   const article = results.docs[0];
   res.send(article);
 });
 
 app.get("/search", async (req, res) => {
-  const results = await searchSolr(req.query.q, req.query.page);
+  const params = {
+    q: req.query.q,
+    "q.op": "AND",
+    qf: "title^10 content^5 tags^3 sections^3 author date id",
+    wt: "json",
+    defType: "edismax",
+    rows: 8,
+    start: req.query.page * 8,
+  };
+  const results = await searchSolr(params);
   res.send(results);
 });
 
