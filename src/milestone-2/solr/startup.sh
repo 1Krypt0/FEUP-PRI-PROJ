53ec3706956a22b5593fb6d670d3651ab1f56cdd
eukia <ritam.mendes@gmail.com>
2022-12-06 11:04:22 +0000

    Fix startup script

--- src/milestone-2/solr/startup.sh
+++ src/milestone-2/solr/startup.sh
@@ -16,13 +16,13 @@ curl -X POST -H 'Content-type:application/json' \
 bin/post -c articles /data/spacenews.json
 
 # Register More Like This request handler
-curl -X POST -H 'Content-type:application/json' -d {
+curl -X POST -H 'Content-type:application/json' -d '{
   "add-requesthandler": {
     "name": "/mlt",
     "class": "solr.MoreLikeThisHandler",
     "defaults": {"mlt.fl": "content"}
   }
-} http://localhost:8983/solr/articles/config
+}' http://localhost:8983/solr/articles/config
 
 # Restart in foreground mode so we can access the interface
 solr restart -f
