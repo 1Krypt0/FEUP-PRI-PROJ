e66c2ae549f9abd0f9e0dd55cf042a28382235d0
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-12-10 11:34:29 +0000

    small fixes to css and app title

--- src/milestone-3/spoogle/client/src/views/HomeView.vue
+++ src/milestone-3/spoogle/client/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="flex items-center justify-center">
     <section class="flex flex-col justify-evenly pb-32 w-1/4 h-3/4">
-      <img src="../assets/logo.svg" class="w-" />
+      <img src="../assets/logo.svg" />
       <SearchBox :query="''" />
     </section>
   </main>
